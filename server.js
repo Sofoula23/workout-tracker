2368eca2d09b53dd162f07d9f7a3e322d7b3e1cb
Sofia <sofimantas@gmail.com>
2021-02-16 22:10:45 -0500

    testing

--- server.js
+++ server.js
@@ -11,16 +11,18 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/workout", 
+app.use(bodyParser.json())
+app.use(require("./routes/htmlRoutes"))
+app.use(require("./routes/apiRoutes"))
+
+Mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/workout", 
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
   });
-app.use(bodyParser.json())
-app.use(require("./routes/htmlRoutes"))
-app.use(require("./routes/apiRoutes"))
+
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`);
 });
